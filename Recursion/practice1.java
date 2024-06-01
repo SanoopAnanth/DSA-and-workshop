3d4b5fa3222aa39ec635a2e73c8a54861f75a16b
SanoopAnanth <sanoopananth2003@gmail.com>
2024-06-01 16:32:43 +0530

    recursion basic

--- Recursion/practice1.java
+++ Recursion/practice1.java
@@ -6,8 +6,14 @@ public class practice1 {
         n_to_1(i+1,n);
         System.out.println(i);
     }
+    public void to_n(int j,int q){
+        if (j<1) return;
+        to_n(j-1,q);
+        System.out.println(j);
+    }
     public static void main(String[] args) {
         practice1 obj=new practice1();
         obj.n_to_1(1,3);
+        obj.to_n(3,3);
     }
 }
