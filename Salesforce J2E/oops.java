2c5120bf243dae31cfca186159ef310fcfb618de
SanoopAnanth <sanoopananth2003@gmail.com>
2024-06-04 22:00:13 +0530

    new addition

---
+++ Salesforce J2E/oops.java
@@ -0,0 +1,28 @@
+public class oops {
+    // protected int age;
+    // protected String name;
+    // protected String section;
+    int x=10;
+    // public void setName(String name){
+    //     this.name=name;
+    // }
+    // public void setAge(int age){
+    //     this.age=age;
+    // }
+    // public void setSection(String section){
+    //     this.section=section;
+    // }
+}
+class derivedOops {
+    public static void main(String[] args) {
+        derivedOops obj=new derivedOops();
+        oops obj1=new oops();
+        // obj.setName("Sanoop");
+        // obj.setAge(21);
+        // obj.setSection("AI & DS C");
+        // System.out.println(obj.name);
+        // System.out.println(obj.section);
+        // System.out.println(obj.age);
+        System.out.println(obj1.x);
+    }
+}
