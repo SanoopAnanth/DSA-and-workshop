72dd1b8b7644e083018489575faf334a0ef047eb
SanoopAnanth <sanoopananth2003@gmail.com>
2024-06-09 15:33:59 +0530

    squareRoot using binary search

---
+++ Salesforce J2E/sqrtBinary.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class sqrtBinary {
+    public static double sqrtBin(int x){
+        double ans=0.0;
+        int left=0;
+        int right=(x/2)+1;
+        while(left<right){
+            int mid=(left+right)/2;
+            int r=mid*mid;
+            if(r==x){
+                ans=mid;
+                break;
+            }
+            if(r>x){
+                right=mid-1;
+            }else{
+                ans=mid;
+                left=mid+1;
+            }
+        }
+        double in=0.1;
+        for(int i=0;i<4;i++){
+            while(ans*ans<=x){
+                ans+=in;
+            }
+            ans-=in;
+            in=in/10;
+        }
+        return (double) ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        double result=sqrtBin(x);
+        System.out.printf("%.3f",result);
+    }
+}
