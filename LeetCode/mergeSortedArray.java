bbd4f41dd9af9406158bd10b53b44da703f3c361
SanoopAnanth <sanoopananth2003@gmail.com>
2024-06-26 11:41:55 +0530

    do not refer this

---
+++ LeetCode/mergeSortedArray.java
@@ -0,0 +1,40 @@
+package LeetCode;
+
+public class mergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int result[]=new int[m+n];
+        int i=0;
+        int j=0;
+        int count=0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                result[count]=nums1[i];
+                i++;
+            }else{
+                result[count]=nums2[j];
+                j++;
+            }
+            count++;
+        }
+        if(j<=n){
+            result[count]=nums2[j];
+            j++;
+            count++;
+        }
+        if(i<=m){
+            result[count]=nums1[i];
+            i++;
+            count++;
+        }
+        for(int k:result){
+            System.out.print(k+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        mergeSortedArray ob=new mergeSortedArray();
+        int nums1[]={1,2,3,0,0,0};
+        int nums2[]={2,5,6};
+        ob.merge(nums1, 3, nums2, 3);
+    }
+}
